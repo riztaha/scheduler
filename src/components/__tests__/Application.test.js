15872dbb49b76d8ea76b70bc66fe12cbf0b55be2
Taha Rizvi <riztaha@gmail.com>
2020-04-22 15:59:19 -0400

    Test Case: add data, books interview, reduces spot

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,23 +1,43 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  prettyDOM,
+  getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
-});
+describe("Application", () => {
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
 
-it("calls the function with specific arguments", () => {
-  const fn = jest.fn();
-  fn(10);
-  expect(fn).toHaveBeenCalledWith(10);
-});
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
-it("uses the mock implementation", () => {
-  const fn = jest.fn((a, b) => 42);
-  fn(1, 2);
-  expect(fn).toHaveReturnedWith(42);
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    //Clicking the add button
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    //Adding the name of the student
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    //Adding name of Interviewer
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    //Clicking the save button
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
+  });
 });
